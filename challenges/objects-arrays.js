8616260e4457dce0469621461517b8aa95a0c771
tanveersaleem786 <tanveer.saleem786@gmail.com>
2019-11-08 20:38:21 -0500

    Refine code in array and obiect challenge

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,16 +76,7 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-
-const uniNames = [];
-const tot_graduates = graduates.length;
-
-for(let i=0; i<tot_graduates;i++) {
-  
-  uniNames.push(graduates[i].university);
-}
-
-const universities = uniNames.sort((x,y) => {
+const universities = graduates.map(graduate => ( graduate.university)).sort((x,y) => {
 
   if (x < y) {
     return -1;
@@ -94,6 +85,7 @@ const universities = uniNames.sort((x,y) => {
   }
 
 });
+
 console.log(universities);
 
 
@@ -103,13 +95,9 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
-for(let i=0; i<tot_graduates;i++) {
-  
-  contactInfo.push(graduates[i].first_name+' '+graduates[i].email);
-
-}
+const contactInfo = graduates.map(graduate => ( `${graduate.first_name} ${graduate.email}`));
 console.log(contactInfo);
+
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
